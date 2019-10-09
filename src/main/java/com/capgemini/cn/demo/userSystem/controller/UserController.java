7c53c70cf449738c1a6df2633d090303a92ce620
kongyujing <652151476@qq.com>
2019-10-09 15:58:39 +0800

    kyj_1009_1

--- src/main/java/com/capgemini/cn/demo/userSystem/controller/UserController.java
+++ src/main/java/com/capgemini/cn/demo/userSystem/controller/UserController.java
@@ -19,6 +19,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
+
 
 /**
  * @author hasaker
